8a857ee4dd66b80a6f2b331f19008ac5e66ea16a
Adrian <156917371+AdrianHdezP@users.noreply.github.com>
2024-11-29 22:58:09 +0100

    Bomba

---
+++ Assets/Scripts/Interactable_Bomb.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Interactable_Bomb : Interactable
+{
+    [SerializeField] private GameObject bomb;
+
+    public override void Interact()
+    {
+        bomb.SetActive(true);
+    }
+}
