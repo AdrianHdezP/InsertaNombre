51943f88b469f65690fa90a83cedb44ce204dc2c
Adrian <156917371+AdrianHdezP@users.noreply.github.com>
2024-11-29 22:35:31 +0100

    Puertas

--- Assets/Scripts/Interactable_Door.cs
+++ Assets/Scripts/Interactable_Door.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class Interactable_Door : Interactable
 {
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     public override void Interact()
     {
-       
+        anim.SetBool("Open", true);
     }
 }
