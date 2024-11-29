3fb2d70878e2fdb7c00cf4f455f2872722198655
Alex <alexconde1809@gmail.com>
2024-11-29 22:45:41 +0100

    Movement IA

--- Assets/EnemyProyectile.cs
+++ Assets/EnemyProyectile.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 public class EnemyProyectile : MonoBehaviour
 {
     [SerializeField] int damage;
+    [SerializeField] float moveSpeed;
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.attachedRigidbody.TryGetComponent(out PlayerMovement move)) move.RecieveDamage(damage);
 
         Destroy(gameObject);
+    }
+
+    public void AddForce(Vector3 direction)
+    {
+        GetComponent<Rigidbody>().AddForce(direction.normalized * moveSpeed);
     }
 }
