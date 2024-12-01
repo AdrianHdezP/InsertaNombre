63e90b03ceabe9ec72564e9159d58c60b92f598c
Alex <alexconde1809@gmail.com>
2024-12-01 03:20:39 +0100

    lighttdssss all of the lights

--- Assets/Scripts/Interactable_Bomb.cs
+++ Assets/Scripts/Interactable_Bomb.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class Interactable_Bomb : Interactable
 {
     [SerializeField] private GameObject bomb;
+    [SerializeField] Light prevLight;
 
     public override void Interact()
     {
         bomb.SetActive(true);
         triggered = true;
+
+        prevLight.enabled = false;
     }
 }
